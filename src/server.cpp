d9fb56330c4967a966d555bd94256b4614d174a6
Jiayou Shi <jyshi@uci.edu>
2018-11-21 01:51:40 +0000

    basic echo not finished

--- src/server.cpp
+++ src/server.cpp
@@ -1,6 +1,12 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "server.h"
 
-#include <iostream>
 
 Server::Server() {
   std::cout << "Server begin construction" << std::endl;
@@ -10,6 +16,40 @@ Server::~Server() {
   std::cout << "Server begin deconstruction" << std::endl;
 }
 
+void Server::Connect() {
+  server_file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_file_descriptor < 0) {
+    perror("server file descriptor");
+  }
+
+  sockaddr_in server_address;
+  memset(&server_address, sizeof(server_address), 0);  
+  server_address.sin_family = AF_INET;
+  server_address.sin_addr.s_addr = htons(INADDR_ANY);
+  server_address.sin_port = htons(8080);
+
+  sockaddr_in server;
+  if (bind(server_file_descriptor, (sockaddr *)&server, sizeof(server)) < 0) {
+    perror("bind");
+  }
+  if (listen(server_file_descriptor, 128) < 0) {
+    perror("listen");
+  }
+}
+
+void Server::Loop() {
+  char message[1024];
+  int client_file_descriptor = accept(server_file_descriptor, (sockaddr *)NULL, NULL);
+  if (client_file_descriptor < 0) {
+    perror("ERROR");
+  }
+
+  std::cout << "connected" << " " << client_file_descriptor << std::endl;
+  read(client_file_descriptor, message, 1024);
+  std::cout << message << std::endl;
+}
+
 void Server::Run() {
-  std::cout << "Run" << std::endl;
+  Connect();
+  Loop();
 }
--- src/server.h
+++ src/server.h
@@ -1,7 +1,15 @@
 class Server {
+  private:
+    int server_file_descriptor;
+
   public:
     Server();
     ~Server();
-
+   
+    
     void Run();
+
+  private:
+    void Connect();
+    void Loop();
 };
